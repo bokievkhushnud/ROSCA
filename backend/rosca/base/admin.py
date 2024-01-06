eebf96b3fcb6a87556a52fbae6fdfbdc8c899585
Khushnud <bokiev.khushnud@mail.ru>
2024-01-06 17:43:51 +0600

    add the db tables

--- backend/rosca/base/admin.py
+++ backend/rosca/base/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Transaction, Loans
 # Register your models here.
+admin.site.register(Transaction)
+admin.site.register(Loans)
--- backend/rosca/base/models.py
+++ backend/rosca/base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    description = models.CharField(max_length=100, blank=True, null=True)
+    category = models.CharField(max_length=100, choices=[('share', 'share'), ('loan', 'loan'), ('voluntary', 'voluntary'), ('penalty', 'penalty'), ('interest', 'interest')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_received = models.DateField()
+
+    def __str__(self):
+        return self.user.username + ' ' + str(self.amount) + ' ' + str(self.date_received)
+
+class Loans(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    description = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    interest_rate = models.FloatField(default=2.0)
+    status = models.CharField(max_length=100, choices=[('pending', 'pending'), ('rejected', 'rejected'), ('paid', 'paid'), ('active', 'active')], default='pending')
+
+    def __str__(self):
+        return self.user.username + ' ' + str(self.amount) + ' ' + str(self.date_received)
+
