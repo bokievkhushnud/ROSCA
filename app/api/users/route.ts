fdea979fcf45c9b11785c5e2fdf5953b33644e7a
bokievkhushnud <bokiev.khushnud@gmail.com>
2025-01-03 16:44:45 +0500

    add api for saving user

---
+++ app/api/users/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const { auth0Id, email, username, firstName, lastName, phone } = await req.json();
+    if (!auth0Id || !email) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    // Check if a user with this sub already exists in DB
+    let user = await prisma.user.findUnique({
+      where: { auth0Id: auth0Id },
+    });
+
+    if (!user) {
+      // Create user
+      user = await prisma.user.create({
+        data: {
+          auth0Id: auth0Id,     // store the Auth0 user_id
+          email: email,
+          username: username,
+          firstName: firstName,
+          lastName: lastName,
+          phone: phone,
+        },
+      });
+    }
+
+    return NextResponse.json({ success: true, user }, { status: 201 });
+  } catch (error) {
+    console.error("Error in POST /auth0-register:", error);
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
